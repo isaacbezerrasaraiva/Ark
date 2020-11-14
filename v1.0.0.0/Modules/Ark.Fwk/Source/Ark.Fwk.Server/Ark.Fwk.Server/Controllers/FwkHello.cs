6e653e9ed8e60e517b63e579228081e63d605b79
Isaac Saraiva <isaacbezerrasaraiva@outlook.com>
2020-11-14 14:13:30 -0300

    1. Solution creation

---
+++ v1.0.0.0/Modules/Ark.Fwk/Source/Ark.Fwk.Server/Ark.Fwk.Server/Controllers/FwkHello.cs
@@ -0,0 +1,27 @@
+﻿// FwkHello.cs
+//
+// This file is integrated part of Ark project
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2020, November 14
+
+using System;
+using System.Xml;
+using System.Data;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ark.Fwk.Server
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FwkHello : ControllerBase
+    {
+        [HttpGet]
+        public String Get()
+        {
+            return "Hello Ark.Fwk.Server!";
+        }
+    }
+}
