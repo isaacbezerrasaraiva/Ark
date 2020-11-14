073953e93f2e4bb4d97f7313482ea2427ff6f684
Isaac Saraiva <isaacbezerrasaraiva@outlook.com>
2020-11-14 15:31:21 -0300

    1. Solution creation

---
+++ v1.0.0.0/Modules/Ark.Fts/Source/Ark.Fts.Server/Ark.Fts.Server/Controllers/FtsHello.cs
@@ -0,0 +1,27 @@
+﻿// FtsHello.cs
+//
+// This file is integrated part of Ark project
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2020, November 14
+
+using System;
+using System.Xml;
+using System.Data;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ark.Fts.Server
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FtsHello : ControllerBase
+    {
+        [HttpGet]
+        public String Get()
+        {
+            return "Hello Ark.Fts.Server!";
+        }
+    }
+}
