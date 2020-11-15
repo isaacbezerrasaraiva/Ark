4e560b3ca9f262b7d0ed5a674610aa8f287a0647
Isaac Saraiva <isaacbezerrasaraiva@outlook.com>
2020-11-14 22:09:30 -0300

    1. Solution creation

---
+++ v1.0.0.0/Modules/Ark/Source/Ark.Server/Ark.Server/Controllers/Hello.cs
@@ -0,0 +1,27 @@
+﻿// Hello.cs
+//
+// This file is integrated part of Ark project
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2020, November 14
+
+using System;
+using System.Xml;
+using System.Data;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ark.Server
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Hello : ControllerBase
+    {
+        [HttpGet]
+        public String Get()
+        {
+            return "Hello Ark.Server!";
+        }
+    }
+}
---
+++ v1.0.0.0/Modules/Ark/Source/Ark.Server/Ark.Server/Startup.cs
@@ -0,0 +1,63 @@
+// Startup.cs
+//
+// This file is integrated part of Ark project
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2020, November 14
+
+using System;
+using System.IO;
+using System.Xml;
+using System.Data;
+using System.Reflection;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ark.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            IMvcBuilder iMvcBuilder = services.AddControllers();
+
+            // Add ark modules as part of the application
+            String[] arkServerAssemblyArray = Directory.GetFiles(".\\Bin\\", "*.Server.dll", SearchOption.AllDirectories);
+            foreach (String arkServerAssembly in arkServerAssemblyArray)
+                iMvcBuilder.AddApplicationPart(Assembly.LoadFrom(arkServerAssembly));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
