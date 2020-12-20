18c3c3a4dd1389a95965aa564bd1fa257346309b
Isaac Saraiva <isaacbezerrasaraiva@outlook.com>
2020-12-20 13:11:59 -0300

    1. Rollback package version to default format 2. Edited PostBuild to the new Release folder structure 3. Added ArkAssemblyResolve function to resolve Libraries dependencies based on Kestrel standards 4. Added officially Ark.Server.runtimeconfig.json and Ark.Server.runtimeconfig.dev.json files to the project with runtime dependency values 5. Added officially Web.config to the project with runtime dependency values

--- v1.0.0.0/Modules/Ark/Source/Ark.Server/Ark.Server/Program.cs
+++ v1.0.0.0/Modules/Ark/Source/Ark.Server/Ark.Server/Program.cs
@@ -7,12 +7,16 @@
 // Created on 2020, November 14
 
 using System;
+using System.IO;
 using System.Xml;
 using System.Data;
+using System.Reflection;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+
+using Ark.Lib;
 
 namespace Ark.Server
 {
@@ -20,14 +24,44 @@ namespace Ark.Server
     {
         public static void Main(string[] args)
         {
+            AppDomain.CurrentDomain.AssemblyResolve += ArkAssemblyResolve;
+
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+        private static Assembly ArkAssemblyResolve(Object sender, ResolveEventArgs args)
+        {
+            String assemblyFolderName = args.Name.Substring(0, args.Name.IndexOf(','));
+            String assemblyVersion = args.Name.Substring(args.Name.IndexOf("Version=") + 8);
+            assemblyVersion = assemblyVersion.Substring(0, assemblyVersion.IndexOf(','));
+            assemblyVersion = assemblyVersion.Substring(0, assemblyVersion.LastIndexOf('.'));
+            String assemblyPath = null;
+
+            if (Directory.Exists(Path.Combine(LibDirectory.Root.Bin.Path, assemblyFolderName)) == true)
+            {
+                assemblyPath = Path.Combine(LibDirectory.Root.Bin.AssemblyFolder[assemblyFolderName].Version[assemblyVersion].Lib.NetCoreApp31.Path, assemblyFolderName) + ".dll";
+
+                if (File.Exists(assemblyPath) == true)
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    return Assembly.LoadFrom(assemblyPath);
+                }
+                else
+                {
+                    assemblyPath = Path.Combine(LibDirectory.Root.Bin.AssemblyFolder[assemblyFolderName].Version[assemblyVersion].Lib.NetStandard20.Path, assemblyFolderName) + ".dll";
+
+                    if (File.Exists(assemblyPath) == true)
+                    {
+                        return Assembly.LoadFrom(assemblyPath);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        }
     }
 }
