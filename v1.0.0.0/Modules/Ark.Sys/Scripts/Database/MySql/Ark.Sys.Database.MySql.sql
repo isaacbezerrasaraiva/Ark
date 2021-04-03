ad10c99d20043d84da53a943b418acacd17222b3
Isaac Saraiva <isaacbezerrasaraiva@outlook.com>
2021-04-03 14:17:42 -0300

    1. Added Role View permissions

--- v1.0.0.0/Modules/Ark.Sys/Scripts/Database/MySql/Ark.Sys.Database.MySql.sql
+++ v1.0.0.0/Modules/Ark.Sys/Scripts/Database/MySql/Ark.Sys.Database.MySql.sql
@@ -18,6 +18,7 @@ use Ark;
 insert into FwkModule (IdDomain, CodModule, Description) values (1, 'Ark.Sys', 'Ark System');
 
 insert into FwkFeature (IdDomain, CodModule, CodFeature, CodModuleBase, CodFeatureBase) values (1, 'Ark.Sys', 'SysRoleServerView', 'Ark.Fwk', 'FwkServerView');
+insert into FwkBranchRoleAction (IdDomain, IdBranch, IdRole, CodModule, CodFeature, CodAction) values (1, 1, 1, 'Ark.Sys', 'SysRoleServerView', 'Init');
 insert into FwkBranchRoleAction (IdDomain, IdBranch, IdRole, CodModule, CodFeature, CodAction) values (1, 1, 1, 'Ark.Sys', 'SysRoleServerView', 'Format');
 insert into FwkBranchRoleAction (IdDomain, IdBranch, IdRole, CodModule, CodFeature, CodAction) values (1, 1, 1, 'Ark.Sys', 'SysRoleServerView', 'ValidateRead');
 insert into FwkBranchRoleAction (IdDomain, IdBranch, IdRole, CodModule, CodFeature, CodAction) values (1, 1, 1, 'Ark.Sys', 'SysRoleServerView', 'Read');
