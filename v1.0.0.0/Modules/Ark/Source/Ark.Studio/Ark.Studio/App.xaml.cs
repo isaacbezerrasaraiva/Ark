bda189328a24a1d21b2612de7ff20e05ec326e2e
Isaac Saraiva <isaacbezerrasaraiva@outlook.com>
2021-06-01 19:45:15 -0300

    1. Added solution model files

--- v1.0.0.0/Modules/Ark/Source/Ark.Studio/Ark.Studio/App.xaml.cs
+++ v1.0.0.0/Modules/Ark/Source/Ark.Studio/Ark.Studio/App.xaml.cs
@@ -8,9 +8,6 @@ using System.Windows;
 
 namespace Ark.Studio
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
--- v1.0.0.0/Modules/Ark/Source/Ark.Studio/Ark.Studio/MainWindow.xaml.cs
+++ v1.0.0.0/Modules/Ark/Source/Ark.Studio/Ark.Studio/MainWindow.xaml.cs
@@ -1,28 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using System.Windows.Documents;
+using System.Windows.Controls;
 using System.Windows.Shapes;
 
 namespace Ark.Studio
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        #region Variables
+        #endregion Variables
+
+        #region Constructors
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        #endregion Constructors
+
+        #region Methods
+        #endregion Methods
+
+        #region Properties
+        #endregion Properties
     }
 }
