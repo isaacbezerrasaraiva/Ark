0b929c33bcf9b596b3ddfc84eb352c451ddc1221
Isaac Saraiva <isaacbezerrasaraiva@outlook.com>
2021-06-08 21:46:37 -0300

    1. Defined some project structure 2. Redesigned main window 3. Added usercontrols that will handle the usability

---
+++ v1.0.0.0/Modules/Ark/Source/Ark.Client/Ark.Client/UserControls/UserControlHome/UserControlHome.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Documents;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace Ark.Client
+{
+    public partial class UserControlHome : UserControl
+    {
+        #region Variables
+        #endregion Variables
+
+        #region Constructors
+
+        public UserControlHome()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Constructors
+
+        #region Methods
+        #endregion Methods
+
+        #region Properties
+        #endregion Properties
+    }
+}
