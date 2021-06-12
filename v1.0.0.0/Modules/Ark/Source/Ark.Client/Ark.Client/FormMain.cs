75e09b736387c1b4d666bf1323d237a31194a786
Isaac Saraiva <isaacbezerrasaraiva@outlook.com>
2021-06-12 14:56:19 -0300

    1. Added Ark.Client based on Windows Forms for .net core to be default client of Ark solution

---
+++ v1.0.0.0/Modules/Ark/Source/Ark.Client/Ark.Client/FormMain.cs
@@ -0,0 +1,37 @@
+﻿// FormMain.cs
+//
+// This file is integrated part of Ark project
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2021, June 12
+
+using System;
+using System.IO;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Ark.Client
+{
+    public partial class FormMain : Form
+    {
+        #region Variables
+        #endregion Variables
+
+        #region Constructors
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Constructors
+
+        #region Methods
+        #endregion Methods
+
+        #region Properties
+        #endregion Properties
+    }
+}
---
+++ v1.0.0.0/Modules/Ark/Source/Ark.Client/Ark.Client/Program.cs
@@ -0,0 +1,28 @@
+// Program.cs
+//
+// This file is integrated part of Ark project
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2021, June 12
+
+using System;
+using System.IO;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Ark.Client
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormMain());
+        }
+    }
+}
