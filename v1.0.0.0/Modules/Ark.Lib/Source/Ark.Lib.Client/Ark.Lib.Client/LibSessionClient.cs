25b7eada0bee6b56923326ebf7419c68b777cf2a
Isaac Saraiva <isaacbezerrasaraiva@outlook.com>
2021-06-13 20:30:36 -0300

    1. Added Session class to handle client shared information

---
+++ v1.0.0.0/Modules/Ark.Lib/Source/Ark.Lib.Client/Ark.Lib.Client/LibSessionClient.cs
@@ -0,0 +1,37 @@
+﻿// LibSessionClient.cs
+//
+// This file is integrated part of Ark project
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2021, June 12
+
+using System;
+using System.IO;
+using System.Xml;
+using System.Data;
+using System.Collections.Generic;
+
+using Lazy;
+
+using Ark.Lib;
+
+namespace Ark.Lib.Client
+{
+    public static class LibSessionClient
+    {
+        #region Variables
+        #endregion Variables
+
+        #region Methods
+        #endregion Methods
+
+        #region Properties
+
+        public static String Environment { get; set; }
+
+        public static String Token { get; set; }
+
+        #endregion Properties
+    }
+}
