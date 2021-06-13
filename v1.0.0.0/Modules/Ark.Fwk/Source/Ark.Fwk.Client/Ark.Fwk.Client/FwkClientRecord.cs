af7c4bc9c74c38cd1210e46309eee6a82ea52f67
Isaac Saraiva <isaacbezerrasaraiva@outlook.com>
2021-06-12 23:17:48 -0300

    1. Added client components

---
+++ v1.0.0.0/Modules/Ark.Fwk/Source/Ark.Fwk.Client/Ark.Fwk.Client/FwkClientRecord.cs
@@ -0,0 +1,51 @@
+﻿// FwkClientRecord.cs
+//
+// This file is integrated part of Ark project
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2021, June 12
+
+using System;
+using System.IO;
+using System.Xml;
+using System.Text;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+using Lazy;
+using Lazy.Forms.Win;
+
+using Ark.Lib;
+using Ark.Lib.Client;
+using Ark.Fwk;
+using Ark.Fwk.Data;
+using Ark.Fwk.Client;
+using Ark.Fwk.IServer;
+
+namespace Ark.Fwk.Client
+{
+    public partial class FwkClientRecord : FwkClientBasic
+    {
+        #region Variables
+        #endregion Variables
+
+        #region Constructors
+
+        public FwkClientRecord()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Constructors
+
+        #region Methods
+        #endregion Methods
+
+        #region Properties
+        #endregion Properties
+    }
+}
