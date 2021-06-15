292a3ab96b1f62256b76fad240d813d3dfe20e79
Isaac Saraiva <isaacbezerrasaraiva@outlook.com>
2021-06-14 23:11:14 -0300

    1. Implemented client rule to authenticate by token or credential

--- v1.0.0.0/Modules/Ark.Fwk/Source/Ark.Fwk.Client/Ark.Fwk.Client/FwkClient.cs
+++ v1.0.0.0/Modules/Ark.Fwk/Source/Ark.Fwk.Client/Ark.Fwk.Client/FwkClient.cs
@@ -64,15 +64,30 @@ namespace Ark.Fwk.Client
 
         protected FwkDataResponse InvokeServer(String endPoint, FwkDataRequest dataRequest, HttpMethod httpMethod)
         {
-            String serverUrl = LibConfigurationClient.DynamicXml["Ark.Lib"]["Settings"]["Environments"][LibSessionClient.Environment]["Server"].Attribute["Url"];
-            
+            String serverUrl = LibConfigurationClient.DynamicXml["Ark.Fwk"]["Providers"][LibSessionClient.Provider]["Server"].Attribute["Url"];
+            String authenticationMode = LibConfigurationClient.DynamicXml["Ark.Fwk"]["Providers"][LibSessionClient.Provider]["Authentication"].Attribute["Mode"];
+
             HttpClient httpClient = new HttpClient();
             httpClient.BaseAddress = new Uri(serverUrl);
-            httpClient.DefaultRequestHeaders.Clear();
-            httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-            httpClient.DefaultRequestHeaders.Add("Culture", LibGlobalization.Culture.Code);
-            httpClient.DefaultRequestHeaders.Add("Token", LibSessionClient.Token);
 
+            httpClient.DefaultRequestHeaders.Clear();
+            httpClient.DefaultRequestHeaders.Add("Culture", LibGlobalization.Culture.Code);
+
+            if (authenticationMode.ToLower() == "token")
+            {
+                httpClient.DefaultRequestHeaders.Add("Token", LibSessionClient.Token);
+            }
+            else if (authenticationMode.ToLower() == "credential")
+            {
+                String databaseAlias = LibConfigurationClient.DynamicXml["Ark.Fwk"]["Providers"][LibSessionClient.Provider]["Database"].Attribute["Alias"];
+                String domainCode = LibConfigurationClient.DynamicXml["Ark.Fwk"]["Providers"][LibSessionClient.Provider]["Domain"].Attribute["Code"];
+
+                httpClient.DefaultRequestHeaders.Add("DatabaseAlias", databaseAlias);
+                httpClient.DefaultRequestHeaders.Add("DomainCode", domainCode);
+                httpClient.DefaultRequestHeaders.Add("Username", LibSessionClient.Username);
+                httpClient.DefaultRequestHeaders.Add("Password", LibSessionClient.Password);
+            }
+            
             String dataRequestString = (String)JsonConvert.SerializeObject(dataRequest, dataRequest.GetType(), null);
             ByteArrayContent dataRequestByteArrayContent = new ByteArrayContent(Encoding.UTF8.GetBytes(dataRequestString));
 
@@ -80,7 +95,7 @@ namespace Ark.Fwk.Client
             httpRequestMessage.Content = dataRequestByteArrayContent;
 
             Task<HttpResponseMessage> httpResponseMessage = httpClient.SendAsync(httpRequestMessage);
-            
+
             String dataResponseString = httpResponseMessage.Result.Content.ReadAsStringAsync().Result;
             return (FwkDataResponse)JsonConvert.DeserializeObject(dataResponseString, this.dataResponseType);
         }
